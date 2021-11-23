bac75b54af59c7e5a456f9a0e8d2e9fd5e4e2ade
Dominik <dominik.stefanik@gmail.com>
2021-11-23 12:12:53 +0000

    findViewById replaced by ViewBinding

--- app/src/main/java/com/dominiks/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/dominiks/recordkeeper/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.dominiks.recordkeeper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.dominiks.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var bottomNav: BottomNavigationView
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //bottomNav = findViewById(R.id.bottom_nav)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bottomNav: BottomNavigationView = binding.bottomNav
+        val frameContent: FrameLayout = binding.frameContent
     }
 }
